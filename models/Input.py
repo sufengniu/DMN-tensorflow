e524ba8f7ff72d5ab3ba71c712a96994aed107f8
Hanyu_Guo <matthewguo123@gmail.com>
2016-05-05 15:27:07 -0400

    trivil changes

---
+++ models/Input.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import random
+
+import numpy as np
+from six.moves import xrange 
+import tensorflow as tf
+
+class Input(object):
+	"""
+	This class reads story sentence by sentence, generating sentence vecter
+	
+	"""
+	def init(self, config):
+		self.vocab_size = config.vocab_size
+		self.batch_size = batch_size
+		self.learning_rate = tf.Variable(float(learning_rate), trainable=False)
+		self.global_step = tf.Variable(0, trainable=False, name='global_step')
+
+		self._input_data = tf.placeholder(tf.int32, ][)
+
+	def step(self, session, config):
+
