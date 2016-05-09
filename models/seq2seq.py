d835a933ae671a262aedd5b9b69c4679afda63e3
Hanyu_Guo <matthewguo123@gmail.com>
2016-05-08 21:35:28 -0400

    seq2seq

--- models/seq2seq.py
+++ models/seq2seq.py
@@ -20,14 +20,19 @@ from tensorflow.python.ops import rnn
 from tensorflow.python.ops import rnn_cell
 from tensorflow.python.ops import variable_scope
 
-def embedding_rnn(encoder_inputs, vocab_size, cell, embedding_size, init_state, dtype=dtypes.float32, scope=None):
+
+
+def sentence_embedding_rnn(encoder_inputs, length, vocab_size, cell, 
+    embedding_size, sentence_length, init_state=None,dtype=dtypes.float32, scope=None):
 	"""
+
 	"""
 	with variable_scope.variable_scope(scope or "embedding_rnn"):
 		encoder_cell = rnn_cell.EmbeddingWrapper(
 				cell, embedding_class=vocab_size,
 				embedding_size=embedding_size)
-		_, encoder_state = rnn.rnn(encoder_cell, encoder_inputs, init_state=init_state, dtype=dtype)
+        for step in range(length):
+		        _, encoder_state = rnn.rnn(encoder_cell, encoder_input[step], encoder_state, dtype=dtype)
 			
 	return encoder_state
 
