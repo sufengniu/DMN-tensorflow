f8e7a09b8459515d529e12146396f3f68b672905
Sufeng Niu <sniu@g.clemson.edu>
2016-06-02 21:57:06 -0400

    add glove downloading

--- get_bAbI.sh
+++ get_bAbI.sh
@@ -6,4 +6,9 @@ tar xvf tasks_1-20_v1-2.tar.gz
 mv tasks_1-20_v1-2 bAbI_data
 rm *.gz
 
+echo "Downloading glove 300d pre-train model..."
+
+wget http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove.6B.zip
+
 echo "Done ."
